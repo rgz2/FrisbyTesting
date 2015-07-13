8270fcd09e3aa7b27a2d3fa594c7936cf50d2dd2
rgz <praigoud@boxtemp.com.br>
2015-07-13 06:57:32 -0300

    new test

--- test_spec.js
+++ test_spec.js
@@ -1,24 +1,42 @@
 var frisby = require('frisby');
-frisby.create('Get Brightbit Twitter feed')
-  .get('https://api.twitter.com/1/statuses/user_timeline.json?screen_name=brightbit')
-  .expectStatus(200)
-  .expectHeaderContains('content-type', 'application/json')
-  .expectJSON('0', {
-    place: function(val) { expect(val).toMatchOrBeNull("Oklahoma City, OK"); }, // Custom matcher callback
-    user: {
-      verified: false,
-      location: "Oklahoma City, OK",
-      url: "http://brightb.it"
-    }
-  })
-  .expectJSONTypes('0', {
-    id_str: String,
-    retweeted: Boolean,
-    in_reply_to_screen_name: function(val) { expect(val).toBeTypeOrNull(String); }, // Custom matcher callback
-    user: {
-      verified: Boolean,
-      location: String,
-      url: String
-    }
-  })
-.toss();
+
+frisby.create('Ensure we are dealing with a teapot')
+  .get('http://httpbin.org/status/418')
+    .expectStatus(418)
+    .expectHeaderContains('X-More-Info', 'http://tools.ietf.org/html/rfc2324')
+.toss()
+
+
+frisby.create('Ensure test has foo and bar')
+
+  .get('http://httpbin.org/get?foo=bar&bar=baz')
+    .expectStatus(200)
+    .expectJSON({
+      args: {
+        foo: 'bar',
+        bar: 'baz'
+      }
+    })
+.toss()
+
+
+frisby.create('Ensure this is *actually* a real teapot, not some imposter coffee pot')
+  .get('http://httpbin.org/status/418')
+    .expectStatus(418)
+    .expectBodyContains('teapot')
+.toss()
+
+frisby.create('Very useful for HTML, text, or raw output')
+  .get('http://asciime.heroku.com/generate_ascii?s=Frisby.js')
+    .inspectBody()
+.toss()
+
+frisby.create('Post JSON string as body')
+    .post('http://httpbin.org/post', {
+        arr: [1, 2, 3, 4],
+        foo: "bar",
+        bar: "baz",
+        answer: 42
+    }, {json: true})
+    .expectHeaderContains('Content-Type', 'json')
+.toss()
